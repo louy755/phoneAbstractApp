4a4dd9ee20f3cf7a8bbe55569d795cb2d3c8156b
Louy Alsadi <louyalsadi@Louys-MacBook-Pro.local>
2020-07-13 16:25:47 -0500

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneAbstractApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
